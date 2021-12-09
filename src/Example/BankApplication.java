04f023a2fa7c8b2d925019e2d8b3ae9f36a9d005
portuna85 <portuna85@gmail.com>
2021-12-09 14:50:37 +0900

    deposit, findAccount 메소드 개발완료하여 커밋 한다.

--- src/Example/BankApplication.java
+++ src/Example/BankApplication.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class BankApplication {
 
-    private static Account[] accountArray = new Account[100];
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Account[] accountArray = new Account[100];
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -72,9 +72,8 @@ public class BankApplication {
         System.out.println("계좌 목록");
         System.out.println("-----------");
 
-        for (int i = 0; i < accountArray.length; i++) {
 
-            Account account = accountArray[i];
+        for (Account account : accountArray) {
 
             if (account != null) {
                 System.out.println(account.getAno() + "  " + account.getOwner() + "  " + account.getBalance());
@@ -84,6 +83,34 @@ public class BankApplication {
 
     // 예금
     private static void deposit() {
+        /*
+            0. 예금 text 출력
+            1. 계좌번호 텍스트 출력
+            2. 계좌 번호 입력
+            3. 2번에서 입력받은 계좌번호를 이용해서 배열 내 계좌 찾기
+                - findAccount 메소드 사용(파라메타 ano, 반환값 Account)
+            4. 예금액 텍스트 출력
+            5. 예금액 입력
+            6. 3번에서 찾은 계좌번호에 5번에서 받은 예금액 입금
+
+            7. 결과성공 택스트 출력
+         */
+        System.out.println("예금");
+        System.out.println("계좌번호 : ");
+        String ano = scanner.next();
+        Account account = findAccount(ano);
+
+        if (account == null) {
+            System.out.println("조회된 계좌가 없없습니다");
+        } else {
+            System.out.println("예금 액 : ");
+            int balance = scanner.nextInt();
+            int accountBalance = account.getBalance();
+
+            account.setBalance(balance + accountBalance);
+            System.out.println("예금에 성공했습니다.");
+
+        }
 
     }
 
@@ -94,6 +121,26 @@ public class BankApplication {
 
     // Account 배열에서 ano와 동일한 Account 찾기
     private static Account findAccount(String ano) {
+
+        /*
+            1. accountArray for 루프를 이용
+            1.1 현재 for 루프에 선택된 account 유효성검사
+            2. accountArray     현재 for 루프에 선택된 getAno 와 전달받은 파라메타 ano 와 비교
+            3. 같으면            현재 for 루프에 선택된 Account를 리턴
+            4. 다르면 accountArray 배열의 길이만큼 반복
+            5. 없으면 null 리턴
+
+            향상된 포문 iter
+         */
+
+        for (Account account : accountArray) {
+
+            if (account != null) {
+                if (account.getAno().equals(ano)) {
+                    return account;
+                }
+            }
+        }
 
         return null;
     }
